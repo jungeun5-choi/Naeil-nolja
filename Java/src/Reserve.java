cba8a69a942dfe9b1b524415cbb2bfc015e145b3
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-07 15:44:37 +0900

    Reserve - 2. 전체 예약 목록 조회, 3. 개별 예약 조회 (parameter: uuid), 4. 예약 취소 (parameter: uuid)
    2. viewAllReservation()
    3. viewMyReservation(UUID myUUID)
    4. cancelMyReservation(UUID myUUID)

--- Java/src/Reserve.java
+++ Java/src/Reserve.java
@@ -16,20 +16,45 @@ public class Reserve {
 
     }
 
+    /* getter */
+    // 1개의 Reservation에서 room 정보만 호출 - uuid 필요
+    public Room getRoom(UUID myUUID) {
+        return reservedRoom.get(myUUID).getRoom();
+    }
+    // 1개의 Reservation에서 room 정보만 호출 - uuid 필요
 
     /* 메서드 */
-    // 예약 생성 - 예약 번호 반환
+    // 1. 예약 생성 - 예약 번호 반환
     public UUID createReservation(Room room, Customer customer){
 
+        // UUID 생성
         UUID uuid = UUID.randomUUID();
+        // 예약 생성
         Reservation reservation = new Reservation(
                 room, customer.getName(), customer.getPhoneNumber(),
                 LocalDateTime.now(), uuid
         );
-
+        
+        // 생성한 예약을 추가
         reservedRoom.put(uuid, reservation);
 
+        // uuid 반환
         return uuid;
+    }
+
+    // 2. 전체 예약 목록 조회
+    public Map<UUID, Reservation> viewAllReservation() {
+        return reservedRoom;
+    }
+    
+    // 3. 개별 예약 조회 (parameter: uuid)
+    public Reservation viewMyReservation(UUID myUUID) {
+        return reservedRoom.get(myUUID);
+    }
+
+    // 4. 예약 취소 (parameter: uuid)
+    public void cancelMyReservation(UUID myUUID) {
+        reservedRoom.remove(myUUID);
     }
 }
 
