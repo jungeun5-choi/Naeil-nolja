4d663d6f5e78e041d5f3a4f9639639fec358bbba
Ahn Jeongmin <symbol9550@gmail.com>
2023-06-07 16:47:55 +0900

    와이어프레임 업데이트 및 호텔 비밀번호 추가

--- Java/src/Hotel.java
+++ Java/src/Hotel.java
@@ -6,34 +6,35 @@ public class Hotel {
     private ArrayList<Room> rooms = new ArrayList<>(); // 객실 여러개
     private int asset; // 보유 자산
     private UUID reservationNumber; // 예약 번호 (uuid)
+    private int hotelPassword;
 
     /* 생성자 */
     public Hotel(){}
-    public Hotel(String hotelName, ArrayList<Room> rooms, int asset, UUID reservationNumber) {
-
+    public Hotel(String hotelName, ArrayList<Room> rooms, int asset, UUID reservationNumber, int hotelPassword) {
         this.hotelName = hotelName;
         this.rooms = rooms;
         this.asset = asset;
         this.reservationNumber = reservationNumber;
+        this.hotelPassword = hotelPassword;
     }
 
     /* getter */
     public String getHotelName() {
         return hotelName;
     }
-
     public int getAsset() {
         return asset;
     }
-
     public ArrayList<Room> getRooms() { // 객실 전체
         return rooms;
     }
     public Room getRoom(int index) { // 객실 한 개
         return rooms.get(index);
     }
-
     public UUID getReservationNumber() {
         return reservationNumber;
+    }
+    public int getHotelPassword(){
+        return hotelPassword;
     }
 }
--- Java/src/HotelReservationApp.java
+++ Java/src/HotelReservationApp.java
@@ -24,8 +24,11 @@ public class HotelReservationApp {
     // 모드 선택
     // 고객, 호텔
     public void selectModeInput(){
-        System.out.println("모드를 선택해주세요.");
-        System.out.printf("%-10s %s\n","1. 고객", "2. 호텔");
+        System.out.print("\n");
+        System.out.println("==========스파르타 호텔==========");
+        System.out.printf("%-10s %-10s %s\n","1. 고객", "2. 호텔","3. 종료");
+        System.out.println("===============================");
+        System.out.print("번호를 입력 하시오 : ");
         int selectMode = sc.nextInt();
         switch(selectMode){
             case 1 : // 고객모드
@@ -34,22 +37,38 @@ public class HotelReservationApp {
             case 2 : // 호텔모드
                 hotelMode();
                 break;
+            case 3 : // 종료
+                System.out.println("프로그램을 종료합니다.");
+                break;
         }
     }
 
     // 1. 고객 모드
     public void customerMode(){
+        System.out.print("\n");
+        System.out.println("===============================");
         System.out.println("스파르타 호텔에 오신것을 환영합니다.");
+        System.out.println("1. 객실 조회");
+        System.out.println("2. 예약 하기");
+        System.out.println("3. 예약 조회");
+        System.out.println("4. 예약 취소");
+        System.out.println("5. 돌아 가기");
+        System.out.println("===============================");
+        System.out.print("번호를 입력 하시오 : ");
         int selectMenu = sc.nextInt();
         switch(selectMenu){
-            case 1 : // 객실 목록 조회
+            case 1 : // 객실 조회
 
-            case 2 : // 예약하기
-
-            case 3 : // 예약조회
-
-            case 4 : // 예약취소
-
+                break;
+            case 2 : // 예약 하기
+                break;
+            case 3 : // 예약 조회
+                break;
+            case 4 : // 예약 취소
+                break;
+            case 5 : // 모드선택으로 돌아가기
+                selectModeInput();
+                break;
         }
     }
 
@@ -63,12 +82,40 @@ public class HotelReservationApp {
 
     // 2. 호텔 모드
     public void hotelMode(){
-
+        System.out.println("관리자 비밀번호를 입력하세요.");
+        String pw = sc.nextLine();
+        // 비밀번호가 같다면 관리자모드로 진입
+        int selectMenu = sc.nextInt();
+        switch(selectMenu){
+            case 1 : // 보유자산 조회
+                hotelAsset();
+                break;
+            case 2 : // 예약목록 조회
+            case 3 : // 남은 객실 조회 > 여건이 된다면?
+            case 4 : // 돌아가기
+                selectModeInput();
+                break;
+        }
     }
 
-    // 2-1 예약목록 조회
-
-    // 2-2 보유자산 조회
+    // 2-1 보유자산 조회
+    public void hotelAsset(){
+        System.out.println("===============================");
+        System.out.println("스파르타 호텔의 총 자산은 : " + hotel.getAsset() + " 입니다.");
+        System.out.printf("%-10s %-10s\n","1. 돌아가기", "2. 종료");
+        System.out.println("===============================");
+        System.out.print("번호를 입력 하시오 : ");
+        int selectMenu = sc.nextInt();
+        switch(selectMenu){
+            case 1 : // 돌아가기
+                hotelMode();
+                break;
+            case 2 : // 종료
+                System.out.println("프로그램을 종료합니다.");
+                break;
+        }
+    }
+    // 2-2 예약목록 조회
 
     // 2-3 남은 객실 조회?
 }
