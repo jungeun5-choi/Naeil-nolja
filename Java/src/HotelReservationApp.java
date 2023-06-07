be933021582dde61d78c2a9b868feece3a931585
Ahn Jeongmin <symbol9550@gmail.com>
2023-06-07 21:11:08 +0900

    와이어 프레임 수정 및 시나리오 구상

--- Java/src/HotelReservationApp.java
+++ Java/src/HotelReservationApp.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.UUID;
 
 public class HotelReservationApp {
     Scanner sc;
-    Hotel hotel;
-    Room room;
-    Reservation reservation;
     Customer customer;
+    Device device;
+    Hotel hotel;
+    Reservation reservation;
+    ReservationContext reservationContext;
+    Reserve reserve;
+    Room room;
+    RoomSize roomSize;
 
     public HotelReservationApp(Scanner sc, Hotel hotel, Room room, Reservation reservation, Customer customer) {
         this.sc = sc;
@@ -63,6 +68,9 @@ public class HotelReservationApp {
             case 2 : // 예약 하기
                 break;
             case 3 : // 예약 조회
+                System.out.print("예약번호를 입력하시오 : ");
+                //uuid 입력 후 맞으면
+//                reserve.viewMyReservation(new UUID());
                 break;
             case 4 : // 예약 취소
                 break;
@@ -74,7 +82,30 @@ public class HotelReservationApp {
 
     // 1-1. 객실 목록 조회
 
+    // 1-2-1. 이름, 번호, 소지금 입력
+    public String enterNameInput(){
+        return sc.next();
+    }
+    public String enterPhoneNumberInput(){
+        return sc.next();
+    }
+    public int enterMoneyInput(){
+        return sc.nextInt();
+    }
+
     // 1-2. 객실 예약하기
+    public void reserveRoom(){
+        System.out.println("이름을 입력하시오 : ");
+        String name = enterNameInput();
+        System.out.println("번호를 입력하시오 : ");
+        String phoneNumber = enterPhoneNumberInput();
+        System.out.println("소지금을 입력하시오 : ");
+        int money = enterMoneyInput();
+        System.out.println("===============================");
+        reserve.viewAllReservation();
+        System.out.println("객실을 선택하시오");
+        reserve.createReservation(room, customer);
+    }
 
     // 1-3. 객실 예약조회
 
@@ -82,17 +113,23 @@ public class HotelReservationApp {
 
     // 2. 호텔 모드
     public void hotelMode(){
+        System.out.print("\n");
         System.out.println("관리자 비밀번호를 입력하세요.");
-        String pw = sc.nextLine();
         // 비밀번호가 같다면 관리자모드로 진입
+        System.out.print("\n");
+        System.out.println("===============================");
+        System.out.printf("%-8s %-8s %s\n","1. 보유자산", "2. 예약목록", "3. 돌아가기");
+        System.out.println("===============================");
+        System.out.print("번호를 입력 하시오 : ");
         int selectMenu = sc.nextInt();
         switch(selectMenu){
             case 1 : // 보유자산 조회
                 hotelAsset();
                 break;
             case 2 : // 예약목록 조회
-            case 3 : // 남은 객실 조회 > 여건이 된다면?
-            case 4 : // 돌아가기
+                reserve.viewAllReservation();
+                break;
+            case 3 : // 돌아가기
                 selectModeInput();
                 break;
         }
@@ -100,10 +137,11 @@ public class HotelReservationApp {
 
     // 2-1 보유자산 조회
     public void hotelAsset(){
+        System.out.print("\n");
         System.out.println("===============================");
         System.out.println("스파르타 호텔의 총 자산은 : " + hotel.getAsset() + " 입니다.");
-        System.out.printf("%-10s %-10s\n","1. 돌아가기", "2. 종료");
         System.out.println("===============================");
+        System.out.printf("%-10s %-10s\n","1. 돌아가기", "2. 종료");
         System.out.print("번호를 입력 하시오 : ");
         int selectMenu = sc.nextInt();
         switch(selectMenu){
