0fd0a34d574fc6910a4be91abd54c91b24829158
Jo Woojin <dnwls3525@gmail.com>
2023-06-08 11:08:06 +0900

    객실 조회, 예약을 위해 선택한  room객체 반환

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Device device = new Device();
         device.inputHotel();
-        device.setup();
+        device.showRoomList();
 
 
     }
